9344a687107ff0fb5df4b5f978775baf823837df
DasPokeks <kruegernorman@web.de>
2016-12-30 15:18:48 +0100

    Exception drum gebastelt, läuft jetzt alles.
    Leider kommt bei heiraten was flasches raus. Guck dir das mal nochmal an :)

--- src/SE/MainTests.java
+++ src/SE/MainTests.java
@@ -3,38 +3,62 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainTests {
-	
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.stage.Stage;
 
-	@Test 
-	public void addMensch() {
-		Mensch Tom = new Mensch("Tom", true);
-		Mensch Lea = new Mensch("Lea", false); 
-		
-		assertEquals("Tom", Tom.name); 
-		assertEquals(true, Tom.bisteM());
-		assertEquals(false, Tom.bisteW()); //Tom ist männlich, daher bei bisteM true und bei bisteW false.
-		
-		assertEquals(true, Lea.bisteW());
-		assertEquals(false, Lea.bisteM()); //Lea ist weiblich, daher bei bisteW true und bei bisteM false.	
-	}
-	
-	@Test
-	public void verheiraten() {
-		Mensch Tom = new Mensch("Tom", true);
-		Mensch Lea = new Mensch("Lea", false);
-		Mensch Sabrina = new Mensch("Sabrina", false);
-		Tom.heirate(Lea); //Tom und Lea heiraten
-		Tom.heirate(Sabrina); //Tom und Sabrina heiraten (fail)
-		
-		assertEquals(Tom, Lea.partner); 
-		assertEquals(Lea, Tom.partner);
-		assertEquals(null, Sabrina.partner); 
-	}
-	
-	@Test
-	public void addKind() {
-		
-	}
-	
+import org.junit.Test;
+
+public class MainTests {
+
+    @Test
+    public void testA() throws InterruptedException {
+        Thread thread = new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                new JFXPanel(); // Initializes the JavaFx Platform
+                Platform.runLater(new Runnable() {
+
+                    @Override
+                    public void run() {
+							try {
+								new Main().start(new Stage());
+							} catch (Exception e) {
+								// TODO Auto-generated catch block
+								System.out.print("eigene Exception: ");
+								e.printStackTrace();
+							}
+						
+                    	
+                    		Mensch Tom = new Mensch("Tom", true);
+                    		Mensch Lea = new Mensch("Lea", false); 
+                    		
+                    		assertTrue("Tom".equals(Tom.name)); 
+                    		assertTrue(Tom.bisteM());
+                    		assertFalse(Tom.bisteW()); //Tom ist männlich, daher bei bisteM true und bei bisteW false.
+                    		
+                    		assertTrue(Lea.bisteW());
+                    		assertFalse(Lea.bisteM()); //Lea ist weiblich, daher bei bisteW true und bei bisteM false.	
+
+                    		Mensch Sabrina = new Mensch("Sabrina", false);
+                    		Tom.heirate(Lea); //Tom und Lea heiraten
+                    		Tom.heirate(Sabrina); //Tom und Sabrina heiraten (fail)
+                    		//System.out.println(Lea.partner.name);
+                    		System.out.println("Tom: " + Tom);
+                    		System.out.println("Lea: " + Lea);
+                    		System.out.println("Leas Partner: " +Lea.partner);
+                    		assertTrue(Tom == Lea.partner); 
+                    		assertTrue(Lea == Tom.partner);
+                    		assertNull(Sabrina.partner); 
+
+                    }
+                });
+            }
+        });
+        thread.start();// Initialize the thread
+        Thread.sleep(10000); // Time to use the app, with out this, the thread
+                                // will be killed before you can tell.
+    }
+
 }
