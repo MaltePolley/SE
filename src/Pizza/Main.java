08aaab36d1da9c63dc159c1d985a4ad61c2d9e3b
DasPokeks <kruegernorman@web.de>
2017-01-30 17:52:05 +0100

    PizzaTests für JUnit und Main als Menüanfang
    Ich muss Main schon erstellen wegen den JUnit-Tests. Sonst hätte das mit dem JavaFX zeug nicht geklappt.
    Hoffe so ist erstmal ok ^^

---
+++ src/Pizza/Main.java
@@ -0,0 +1,10 @@
+import javafx.stage.Stage;
+
+public class Main {
+
+	public void start(Stage stage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/Pizza/PizzaTests.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.stage.Stage;
+import java.util.ArrayList;
+
+public class PizzaTests {
+	    @Test
+	    public void testA() throws InterruptedException {
+	        Thread thread = new Thread(new Runnable() {
+
+	            @Override
+	            public void run() {
+	                new JFXPanel(); // Initializes the JavaFx Platform
+	                Platform.runLater(new Runnable() {
+
+	                    @Override
+	                    public void run() {
+								try {
+									new Main().start(new Stage());
+								} catch (Exception e) {
+									// TODO Auto-generated catch block
+									System.out.print("eigene Exception: ");
+									e.printStackTrace();
+								}
+								
+								//TODO hier Tests einfügen
+								
+	                    }
+	                });
+	            }
+	        });
+	        thread.start();// Initialize the thread
+	        Thread.sleep(10000); // Time to use the app, with out this, the thread
+	                                // will be killed before you can tell.
+	    }
+
+}
+	
