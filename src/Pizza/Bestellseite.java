9a9eb904cdeb11f3cf2bf5e26eef71e58529c781
Malte Polley <malte-polley@web.de>
2017-02-04 11:21:44 +0100

    Bisschen weiter gemacht, man kann nun die Seiten aufrufen, nur die Daten speicehrn und ein paar Anzeigen fehlen

--- src/Pizza/Bestellseite.java
+++ src/Pizza/Bestellseite.java
@@ -1,5 +1,6 @@
 package Pizza;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -7,18 +8,21 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Bestellseite implements Initializable {
 
 	public Stage primaryStage;
+	FXMLLoader loader = new FXMLLoader();
 	Kunde currentK;
-	Order currentO;
+	Order currentO = new Order();
 
 	@FXML
 	TextField vorname;
@@ -58,6 +62,22 @@ public class Bestellseite implements Initializable {
 
 	@FXML
 	Label text;
+	
+	@FXML
+	Button back;
+	
+	@FXML
+	protected void getBack(){
+		try {
+			AnchorPane menu = (AnchorPane) loader.load(getClass().getResource("Haupt.fxml"));
+			loader.setLocation(getClass().getResource("org/ttr/blue/view/menu.fxml"));
+			Scene sceneMenu = new Scene(menu);
+			Main.getPrimaryStage().setScene(sceneMenu);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
 
 	@FXML
 	protected void getKData() {
@@ -77,10 +97,11 @@ public class Bestellseite implements Initializable {
 		PizzaArt pa = (PizzaArt) pizza.getValue();
 		PizzaGröße pg = (PizzaGröße) pizzaGr.getValue();
 		// TODO Toppings
-		//ArrayList<Toppings> tops = toppings.getSelectionModel().ge
 		ArrayList<Toppings> tops = new ArrayList<Toppings>();
+		tops.add((Toppings) toppings.getValue());
 		Pizza newPizza = new Pizza(pa, pg, tops);
 		currentO.pizzen.add(newPizza);
+	
 		fillText();
 	}
 
@@ -102,23 +123,27 @@ public class Bestellseite implements Initializable {
 	
 	public void fillText(){
 		// Display
-					text.setText(currentK.vorname + " " + currentK.nachname + "\n");
+		text.setText("");
+				if(currentK!=null){
+					text.setText(text.getText() + currentK.vorname + " " + currentK.nachname + "\n");
 					text.setText(text.getText() + currentK.straße + "\n");
 					text.setText(text.getText() + currentK.ort + "\n");
 					text.setText(text.getText() + currentK.handynummer + "\n");
 					text.setText(text.getText() + currentK.plz + "\n");
+				}
 					if (currentO != null && currentO.pizzen != null) {
 						text.setText(text.getText() + "\n");
 						for (int i = 0; i < currentO.pizzen.size(); i++) {
-							text.setText(text.getText() + currentO.pizzen.get(i));
+							text.setText(text.getText() + currentO.pizzen.get(i).pizzaArt.toString() + "-" +  currentO.pizzen.get(i).pg.toString());
 							if (currentO.pizzen.get(i).toppings != null) {
 								for (int x = 0; x < currentO.pizzen.get(i).toppings.size(); x++) {
-									text.setText(text.getText() + currentO.pizzen.get(i).toppings.get(x));
+									text.setText(text.getText() + "\n"+ currentO.pizzen.get(i).toppings.get(x).toString());
 								}
 							}
 							text.setText(text.getText() + "\n");
 						}
 
 					}
+					// TODO Preis setzen
 	}
 }
--- src/Pizza/Kochseite.java
+++ src/Pizza/Kochseite.java
@@ -1,15 +1,23 @@
 package Pizza;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Kochseite implements Initializable{
+	
+	public Stage primaryStage;
+	FXMLLoader loader = new FXMLLoader();
 	
 	@FXML
 	ComboBox orders;
@@ -27,6 +35,22 @@ public class Kochseite implements Initializable{
 	Button ok;
 	
 	@FXML
+	Button back;
+	
+	@FXML
+	protected void getBack(){
+		try {
+			AnchorPane menu = (AnchorPane) loader.load(getClass().getResource("Haupt.fxml"));
+			loader.setLocation(getClass().getResource("org/ttr/blue/view/menu.fxml"));
+			Scene sceneMenu = new Scene(menu);
+			Main.getPrimaryStage().setScene(sceneMenu);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	@FXML
 	protected void display(){
 		// TODO Datei auslesen und text + status setzen
 	}
@@ -35,7 +59,7 @@ public class Kochseite implements Initializable{
 	protected void setStatus(){
 		// TODO status setzen je nach Combobox auswahl
 	}
-
+	
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Initialisieren
